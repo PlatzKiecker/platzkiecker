40c5101c6cf8a8172ddc5c107a5c177ba78fe26f
aaron <92885159+aaronroters@users.noreply.github.com>
2024-05-13 14:33:41 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,10 +22,10 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Coming Soon!
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">Under Construction</p>
     </>
   );
 }
