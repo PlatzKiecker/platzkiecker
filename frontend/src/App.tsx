c610aef31c8e78eb93951d7cf0bae1471e94c925
appl3burg3r <ricardofricke10@gmail.com>
2024-06-04 20:33:17 +0200

    Update: Clean up default app vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
   // test
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>PlatzKiecker</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 10)}>
-          count is {count}
-        </button>
-        <p>
-          Coming Soon!
-        </p>
-      </div>
-      <p className="read-the-docs">Under Construction</p>
-    </>
-  );
+  return <>helölo</>;
 }
-
-export default App;
