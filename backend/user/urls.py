06a1d67c43e6a0f7f215d7ad07a392f0a6f9c68f
aaronroters <aaronroters@googlemail.com>
2024-06-04 14:15:43 +0200

    implemented user endpoints

---
+++ backend/user/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.user_registration_view, name='register'),
+    path('login/', views.user_login_view, name='login'),
+]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, authentication, status
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+#@renderer_classes([JSONRenderer])
+def user_registration_view(request):
+    if request.method == 'POST':
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['GET', 'POST'])
+#@renderer_classes([JSONRenderer])
+def user_login_view(request):
+    if request.method == 'POST':
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
