da1056eeedcd3599cfba58dd253035596351bc94
aaronroters <aaronroters@googlemail.com>
2024-06-04 15:32:37 +0200

    added redirects

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -43,15 +43,3 @@ class UserLoginSerializer(serializers.Serializer):
 
         attrs['user'] = user
         return attrs
-
-from django.contrib.auth.models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
-from rest_framework import generics, permissions, authentication, status
-from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserSerializer
+from rest_framework import generics, authentication, status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import JSONRenderer
-from rest_framework.views import APIView
 from django.contrib.auth import logout
-
-from rest_framework import generics
+from django.shortcuts import redirect
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
 from .models import User
+
 
 
 class UserRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED) and redirect('login')
 
 class UserLoginView(generics.GenericAPIView):
     serializer_class = UserLoginSerializer
@@ -22,11 +24,11 @@ class UserLoginView(generics.GenericAPIView):
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK) and redirect('http://localhost:3000')
 
 class UserLogoutView(generics.DestroyAPIView):
     queryset = User.objects.all()
     
     def get(self, request):
         logout(request)
-        return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_200_OK) and redirect('login')
