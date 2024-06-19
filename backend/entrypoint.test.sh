c171dc50a6abeb9d49d332b5ad09d051edb6f1d5
aaronroters <aaronroters@googlemail.com>
2024-06-19 22:26:10 +0200

    fixed integration test pipeline

---
+++ backend/entrypoint.test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py test user
+python manage.py test restaurant
+python manage.py test booking
+
+exec "$@"
