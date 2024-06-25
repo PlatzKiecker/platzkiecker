edd4fe1b9c4f8403dcbb036234c7cbdbcae4f0d7
aaron <92885159+aaronroters@users.noreply.github.com>
2024-06-25 22:35:56 +0200

    Swagger (#93)
    * added swagger
    
    * added swagger
    
    * added staticfiles for swagger
    
    * added staticfiles for swagger
    
    * added staticfiles for swagger
    
    * added staticfiles for swagger
    
    * automated staticfile collection
    
    * automated staticfile collection
    
    * fixed everything
    
    * fixed everything
    
    * fixed everything

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "sqllite3"),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
         "USER": os.environ.get("SQL_USER", "user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
@@ -163,3 +163,11 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'basic': {
+            'type': 'basic'
+        }
+    },
+}
