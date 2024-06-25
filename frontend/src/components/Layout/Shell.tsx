d2414fc0b66e38f40054fc19c0b525dda62e8628
appl3burg3r <ricardofricke10@gmail.com>
2024-06-25 12:30:56 +0200

    Remove: user const

--- frontend/src/components/Layout/Shell.tsx
+++ frontend/src/components/Layout/Shell.tsx
@@ -1,11 +1,6 @@
 import { Menu, MenuButton, MenuItem, MenuItems, Transition } from "@headlessui/react";
 import { NavLink, Outlet } from "react-router-dom";
 
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
 const navigation = [
   { name: "Dashboard", href: "/" },
   { name: "Settings", href: "/settings" },
