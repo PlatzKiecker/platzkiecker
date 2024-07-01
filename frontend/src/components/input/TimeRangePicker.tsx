510df5e901ae6fcd835c1f862cbcadcd69f11d1d
appl3burg3r <ricardofricke10@gmail.com>
2024-07-01 11:11:08 +0200

    Refactor: put settings section into seperate components

--- frontend/src/components/input/TimeRangePicker.tsx
+++ frontend/src/components/input/TimeRangePicker.tsx
@@ -5,7 +5,7 @@ export default function TimeRangePicker({ value }: { value: BookingPeriod }) {
   return (
     <div className="flex items-center">
       <InputField type="time" />
-      <span className="mx-2">to</span>
+      <span className="mx-2 text-sm text-gray-700">to</span>
       <InputField type="time" />
     </div>
   );
--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,14 +1,8 @@
 import Page from "../components/layout/Page";
 import SettingsLayout from "../components/layout/SettingsLayout";
 import InputField from "../components/input/InputField";
-import DateRangePicker from "../components/input/DateRangePicker";
-import { useState } from "react";
-import { DateValueType } from "react-tailwindcss-datepicker";
-import Button from "../components/input/Button";
-import mySWR from "../utils/mySWR";
-import TimeRangePicker from "../components/input/TimeRangePicker";
-import { BookingPeriod, BookingPeriods } from "../types/bookings";
-import { VacationPeriod } from "../types/vacations";
+import BookingPeriodsSection from "../components/pages/settings/BookingPeriodsSection";
+import VacationPeriodsSection from "../components/pages/settings/VacationPeriodsSection";
 
 export default function Settings() {
   return (
@@ -25,120 +19,5 @@ export default function Settings() {
         </SettingsLayout.Section>
       </SettingsLayout>
     </Page>
-  );
-}
-
-function BookingPeriodsSection() {
-  const { data, error, loading } = mySWR(`/booking-periods/list/`);
-  const [days, setPeriods] = useState<BookingPeriods>({
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: [],
-    saturday: [],
-    sunday: [],
-  });
-
-  const handleValueChange = (value: DateValueType, id: number) => {
-    console.log(value);
-  };
-
-  const handleAddPeriod = (day: "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday" | "sunday") => {
-    setPeriods((prev) => {
-      // POST to backend
-      console.log("POST to backend", day);
-
-      const newPeriod = { id: 1, value: { startTime: new Date(), endTime: new Date() } };
-      return { ...prev, [day]: [...prev[day], newPeriod] };
-    });
-  };
-
-  const dayJSX = Object.entries(days).map(([day, value]) => {
-    return (
-      <div key={day} className="space-y-1">
-        <h3 className="font-medium">{day}</h3>
-        {value.map((period: BookingPeriod) => period.value.startTime && period.value.endTime && <TimeRangePicker key={period.value.startTime.toLocaleString()} value={period} />)}
-        <Button variant="secondary" onClick={() => handleAddPeriod(day as keyof BookingPeriods)}>
-          +
-        </Button>
-      </div>
-    );
-  });
-
-  return <div className="space-y-4">{dayJSX}</div>;
-}
-
-function VacationPeriodsSection() {
-  const { data, error, loading } = mySWR(`/vacations/list/`);
-  console.log(data, loading, error);
-
-  const [periods, setPeriods] = useState<VacationPeriod[]>([
-    {
-      id: 1,
-      value: {
-        startDate: new Date(),
-        endDate: new Date(),
-      },
-    },
-    {
-      id: 2,
-      value: {
-        startDate: new Date(),
-        endDate: new Date(),
-      },
-    },
-  ]);
-
-  const handleValueChange = (value: DateValueType, id: number) => {
-    console.log(value);
-
-    if (!value?.startDate || !value?.endDate) {
-      // TODO: delete period
-      setPeriods((prevPeriods: VacationPeriod[]) => {
-        const updatedPeriods = prevPeriods.filter((period) => period.id !== id);
-        return updatedPeriods;
-      });
-    } else {
-      // TODO: update period
-      setPeriods((prevPeriods: VacationPeriod[]) => {
-        const updatedPeriods = prevPeriods.map((period) => {
-          if (period.id === id && value)
-            return {
-              id: period.id,
-              value: {
-                startDate: value.startDate,
-                endDate: value.endDate,
-              },
-            };
-          return period;
-        });
-        return updatedPeriods;
-      });
-    }
-  };
-
-  const handleAddPeriod = () => {
-    setPeriods((prev) => {
-      const newPeriod = { id: prev.length + 1, value: { startDate: new Date(), endDate: new Date() } };
-      return [...prev, newPeriod];
-    });
-  };
-
-  const periodJSX = periods.map((period) => {
-    return (
-      <div key={period.id}>
-        <DateRangePicker value={period.value} onChange={(newValue) => handleValueChange(newValue, period.id)} />
-      </div>
-    );
-  });
-
-  return (
-    <div className="space-y-4">
-      {periodJSX}
-      <Button variant="secondary" onClick={handleAddPeriod}>
-        +
-      </Button>
-    </div>
   );
 }
