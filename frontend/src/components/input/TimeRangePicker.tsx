ea04757b443ea2053446e08732c2cef9a93d58b6
appl3burg3r <ricardofricke10@gmail.com>
2024-07-08 15:26:19 +0200

    Add: booking period create backend integration

--- frontend/src/components/input/TimeRangePicker.tsx
+++ frontend/src/components/input/TimeRangePicker.tsx
@@ -4,9 +4,9 @@ import { BookingPeriod } from "../../types/bookings";
 export default function TimeRangePicker({ value }: { value: BookingPeriod }) {
   return (
     <div className="flex items-center">
-      <InputField type="time" />
+      <InputField type="time" value={value.open.toString()} />
       <span className="mx-2 text-sm text-gray-700">to</span>
-      <InputField type="time" />
+      <InputField type="time" value={value.close.toString()} />
     </div>
   );
 }
--- frontend/src/components/pages/settings/BookingPeriodsSection.tsx
+++ frontend/src/components/pages/settings/BookingPeriodsSection.tsx
@@ -19,9 +19,16 @@ export default function BookingPeriodsSection() {
 
   useEffect(() => {
     if (data) {
-      console.log(data);
-
-      // setPeriods(data);
+      const periods: BookingPeriods = {
+        monday: data.filter((period: BookingPeriod) => period.weekday === "MO") || [],
+        tuesday: data.filter((period: BookingPeriod) => period.weekday === "TU") || [],
+        wednesday: data.filter((period: BookingPeriod) => period.weekday === "WE") || [],
+        thursday: data.filter((period: BookingPeriod) => period.weekday === "TH") || [],
+        friday: data.filter((period: BookingPeriod) => period.weekday === "FR") || [],
+        saturday: data.filter((period: BookingPeriod) => period.weekday === "SA") || [],
+        sunday: data.filter((period: BookingPeriod) => period.weekday === "SU") || [],
+      };
+      setPeriods(periods);
     }
   }, [data]);
 
@@ -34,7 +41,7 @@ export default function BookingPeriodsSection() {
     console.log(response);
 
     setPeriods((prev) => {
-      const newPeriod = { id: 1, value: { startTime: new Date(), endTime: new Date() } };
+      const newPeriod = { id: 1, value: { open: new Date(), close: new Date() } };
       return { ...prev, [day]: [...prev[day], newPeriod] };
     });
   };
@@ -43,7 +50,7 @@ export default function BookingPeriodsSection() {
     return (
       <div key={day} className="space-y-1">
         <h3 className="font-medium">{day}</h3>
-        {value.map((period: BookingPeriod) => period.value.startTime && period.value.endTime && <TimeRangePicker key={period.value.startTime.toLocaleString()} value={period} />)}
+        {value.map((period: BookingPeriod) => period.open && period.close && <TimeRangePicker key={period.open.toString()} value={period} />)}
         <Button variant="secondary" onClick={() => handleAddPeriod(day as keyof BookingPeriods)}>
           +
         </Button>
